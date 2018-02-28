20657bbfbbc69961a51a153daad2e53d865da1c7
Alexey Ostapenko <alexeyostapenko@Air-Alexey.Dlink>
2018-02-28 15:09:25 +0300

    brackets sequence

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+ let strl = str.length;
+ 
+  if (strl%2 !== 0)
+  return false;
+
+  let arrl = bracketsConfig.length;
+  let opener =[];
+  let closer = [];
+  bracketsConfig.forEach((x) => {opener.push(x[0]); closer.push(x[1])})
+			for (i = 0; i<strl; i++)
+			{
+				for (j =0; j < arrl; j++)
+				{
+          if(str[i] === opener[j] && str[i+1] === closer[j])
+          {
+            str = str.substring(0, i) + str.substring(i+2);
+					  i = -1;
+          }
+        } 
+			}
+
+      if (str.length === 0)
+      return true;
+      if (str.length > 0)
+      return false;
+
 }
